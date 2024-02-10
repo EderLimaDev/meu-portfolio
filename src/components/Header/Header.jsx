55505da071272e5e8a6ae468f2549ab4e9ee93be
EderLimaDev <ederlimarh@gmail.com>
2024-02-09 21:39:55 -0300

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,25 @@
+
+
+export const Header = () => {
+  return (
+    <header className="w-full text-blue-50 flex justify-between h-13">
+        
+        <div className="text-sm m-10">
+          Logo
+        </div>
+
+        <nav className='flex'>
+            <ul className="flex m-10 w-[300px] justify-between">
+                <li>Home</li>
+                <li>Sobre</li>
+                <li>Projetos</li>
+                <li>Redes</li>
+            </ul>
+            <a href="#"></a>
+            <span></span>
+        </nav>
+        
+
+    </header>
+  )
+}
